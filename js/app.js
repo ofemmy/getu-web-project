5438d2852507645192fd302461cd663e09bef2fa
Oladayo Fagbemi <oofagbemi@outlook.com>
2020-09-23 05:28:50 +0200

    complete setup with tailwind

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
 hamburger?.addEventListener("click", function () {
-    if (menu.classList.contains("hidden")) {
-        menu.classList.remove("hidden")
-    } else {
-        menu.classList.add("hidden")
-    }
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
+
+//scrolll to top
+const btn = document.querySelector("#scrollTop");
+document?.addEventListener("click", function () {
+  window.scrollTo(0, 0);
 });
